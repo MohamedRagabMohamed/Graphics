b86d4ed3eeaa9d67d736b38a8e7a7aa152dc66ac
mohamed ragab <mohamed225.mr@gmail.com>
2017-12-18 13:31:16 +0200

    First commit

---
+++ main.cpp
@@ -0,0 +1,190 @@
+#if defined(UNICODE) && !defined(_UNICODE)
+#endif
+
+#include <tchar.h>
+#include <windows.h>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+#define MAXENTRIES 500
+
+struct EdgeRec
+{
+    double x;
+    double minv;
+    int ymax;
+    bool operator<(EdgeRec r)
+    {
+        return x<r.x;
+    }
+};
+
+typedef list<EdgeRec> EdgeList;
+
+
+EdgeRec InitEdgeRec(POINT& v1,POINT& v2)
+{
+    if(v1.y>v2.y) swap(v1,v2);
+    EdgeRec rec;
+    rec.x=v1.x;
+    rec.ymax=v2.y;
+    rec.minv=(double)(v2.x-v1.x)/(v2.y-v1.y);
+    return rec;
+}
+
+
+void InitEdgeTable(POINT *polygon,int n,EdgeList table[])
+{
+    POINT v1=polygon[n-1];
+    for(int i=0; i<n; i++)
+    {
+        POINT v2=polygon[i];
+        if(v1.y==v2.y)
+        {
+            v1=v2;
+            continue;
+        }
+        EdgeRec rec=InitEdgeRec(v1, v2);
+        table[v1.y].push_back(rec);
+        v1 = polygon[i];
+    }
+}
+
+
+
+void GeneralPolygonFill(HDC hdc,POINT *polygon,int n,COLORREF c)
+{
+    EdgeList *table=new EdgeList [MAXENTRIES];
+    InitEdgeTable(polygon,n,table);
+    int y = 0;
+    while(y < MAXENTRIES && table[y].size() == 0) y++;
+    if(y==MAXENTRIES)return;
+    EdgeList ActiveList=table[y];
+    while (ActiveList.size()>0)
+    {
+        ActiveList.sort();
+        for(EdgeList::iterator it = ActiveList.begin(); it!=ActiveList.end(); it++)
+        {
+            int x1=(int)ceil(it->x);
+            it++;
+            int x2=(int)floor(it->x);
+            for(int x = x1 ; x <= x2; x+=2)
+                SetPixel(hdc,round(x),y,c);
+        }
+        y++;
+        EdgeList::iterator it=ActiveList.begin();
+        while(it!=ActiveList.end())
+            if(y==it->ymax)
+                it = ActiveList.erase(it);
+            else it++;
+        for(EdgeList::iterator it=ActiveList.begin(); it!=ActiveList.end(); it++)    it->x+=it->minv;
+        ActiveList.insert(ActiveList.end(),table[y].begin(),table[y].end());
+    }
+    delete[] table;
+}
+
+
+
+
+/*  Declare Windows procedure  */
+LRESULT CALLBACK WindowProcedure (HWND, UINT, WPARAM, LPARAM);
+
+/*  Make the class name into a global variable  */
+TCHAR szClassName[ ] = _T("CodeBlocksWindowsApp");
+
+int WINAPI WinMain (HINSTANCE hThisInstance,
+                    HINSTANCE hPrevInstance,
+                    LPSTR lpszArgument,
+                    int nCmdShow)
+{
+    HWND hwnd;               /* This is the handle for our window */
+    MSG messages;            /* Here messages to the application are saved */
+    WNDCLASSEX wincl;        /* Data structure for the windowclass */
+
+    /* The Window structure */
+    wincl.hInstance = hThisInstance;
+    wincl.lpszClassName = szClassName;
+    wincl.lpfnWndProc = WindowProcedure;      /* This function is called by windows */
+    wincl.style = CS_DBLCLKS;                 /* Catch double-clicks */
+    wincl.cbSize = sizeof (WNDCLASSEX);
+
+    /* Use default icon and mouse-pointer */
+    wincl.hIcon = LoadIcon (NULL, IDI_APPLICATION);
+    wincl.hIconSm = LoadIcon (NULL, IDI_APPLICATION);
+    wincl.hCursor = LoadCursor (NULL, IDC_ARROW);
+    wincl.lpszMenuName = NULL;                 /* No menu */
+    wincl.cbClsExtra = 0;                      /* No extra bytes after the window class */
+    wincl.cbWndExtra = 0;                      /* structure or the window instance */
+    /* Use Windows's default colour as the background of the window */
+    wincl.hbrBackground = (HBRUSH) COLOR_BACKGROUND;
+
+    /* Register the window class, and if it fails quit the program */
+    if (!RegisterClassEx (&wincl))
+        return 0;
+
+    /* The class is registered, let's create the program*/
+    hwnd = CreateWindowEx (
+               0,                   /* Extended possibilites for variation */
+               szClassName,         /* Classname */
+               _T("Code::Blocks Template Windows App"),       /* Title Text */
+               WS_OVERLAPPEDWINDOW, /* default window */
+               CW_USEDEFAULT,       /* Windows decides the position */
+               CW_USEDEFAULT,       /* where the window ends up on the screen */
+               544,                 /* The programs width */
+               375,                 /* and height in pixels */
+               HWND_DESKTOP,        /* The window is a child-window to desktop */
+               NULL,                /* No menu */
+               hThisInstance,       /* Program Instance handler */
+               NULL                 /* No Window Creation data */
+           );
+
+    /* Make the window visible on the screen */
+    ShowWindow (hwnd, nCmdShow);
+
+    /* Run the message loop. It will run until GetMessage() returns 0 */
+    while (GetMessage (&messages, NULL, 0, 0))
+    {
+        /* Translate virtual-key messages into character messages */
+        TranslateMessage(&messages);
+        /* Send message to WindowProcedure */
+        DispatchMessage(&messages);
+    }
+
+    /* The program return-value is 0 - The value that PostQuitMessage() gave */
+    return messages.wParam;
+}
+
+
+/*  This function is called by the Windows function DispatchMessage()  */
+int c = 0 ;
+int xs = 0 , xe = 0 , xc = 0 , ys = 0 ,  yc = 0 ,ye = 0;
+POINT polg[100];
+HDC hdc;
+LRESULT CALLBACK WindowProcedure (HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    switch (message)                  /* handle the messages */
+    {
+    case WM_LBUTTONDOWN:
+            polg[c].x = LOWORD(lParam);
+            polg[c].y = HIWORD(lParam);
+            hdc = GetDC(hwnd);
+            c++;
+            break;
+    case WM_RBUTTONDOWN:
+            polg[c].x = LOWORD(lParam);
+            polg[c].y = HIWORD(lParam);
+            hdc = GetDC(hwnd);
+            GeneralPolygonFill(hdc , polg , ++c , RGB(19,15,23));
+            c=0;
+        break;
+    case WM_DESTROY:
+        PostQuitMessage (0);       /* send a WM_QUIT to the message queue */
+        break;
+    default:                      /* for messages that we don't deal with */
+        return DefWindowProc (hwnd, message, wParam, lParam);
+    }
+
+    return 0;
+}
